3ab21b7278a88f7b7b48f64694b592d00a1b9fda
wweerrbb <ysq13579@hotmail.com>
2021-12-09 23:08:47 -0500

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Dim Sum with family',
+    day: 'June 6th at 12:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Movie "Matrix 4 with my wife',
+    day: 'September 11th at 18:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Monitoring the Crypto Market',
+    day: 'December 23rd at 8:00am',
+    reminder: false,
+  },
+];
